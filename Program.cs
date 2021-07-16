d4167fe2dc6dde69f34e8d8c374fb062509e82b6
Carlos Oliveira <oslcarlos@gmail.com>
2021-07-15 23:08:10 -0300

    Atribuiçoes de variaveis

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuiçoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execução do projeto 6 atribuiçoes de variaveis");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 39;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
